3bc9fffa647be97c139acc3d6b5b14334d6d9ba8
NeonKnightOA <openarena.arg@gmail.com>
2019-09-23 12:09:23 -0300

    Added building scripts.

---
+++ build-pack.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem -----------------------------------------------------
+rem This script assumes that you have zip installed
+rem and it is in your path.  If zip is not installed
+rem this script will not work.  Please check your distro's
+rem documentation on how to install the zip package.
+rem -----------------------------------------------------
+
+echo Compressing...
+cd baseoa
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 levelshots\*.jpg
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 levelshots\*.tga
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 maps\*.aas
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 maps\*.bsp
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 models\*.*
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 scripts\*.arena
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 scripts\*.shader
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 sound\*.*
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 textures\*.*
+..\zip.exe -9r ..\z_oacmp2_beta.pk3 *.cfg
+echo Done
+
+@echo on
---
+++ build-pack.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# -----------------------------------------------------
+# this script assumes that you have zip installed
+# and it is in your path.  If zip is not installed
+# this script will not work.  Please check your distro's
+# documentation on how to install the zip package.
+# -----------------------------------------------------
+echo Compressing...
+cd baseoa
+zip -9r ..\z_oacmp2_beta.pk3 levelshots\*.jpg
+zip -9r ..\z_oacmp2_beta.pk3 levelshots\*.tga
+zip -9r ..\z_oacmp2_beta.pk3 maps\*.aas
+zip -9r ..\z_oacmp2_beta.pk3 maps\*.bsp
+zip -9r ..\z_oacmp2_beta.pk3 models\*.*
+zip -9r ..\z_oacmp2_beta.pk3 scripts\*.arena
+zip -9r ..\z_oacmp2_beta.pk3 scripts\*.shader
+zip -9r ..\z_oacmp2_beta.pk3 sound\*.*
+zip -9r ..\z_oacmp2_beta.pk3 textures\*.*
+zip -9r ..\z_oacmp2_beta.pk3 *.cfg
+echo Done
+
+exit 0
